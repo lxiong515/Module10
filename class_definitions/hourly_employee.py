fb1076d2a4f47468d7653803952388c012e783dc
lxiong515 <58718122+lxiong515@users.noreply.github.com>
2020-07-08 22:29:41 -0500

    final versions of salaried and hourly employee derived classes

--- class_definitions/hourly_employee.py
+++ class_definitions/hourly_employee.py
@@ -1,5 +1,5 @@
 """
-Program: salaried_employee.py
+Program: hourly_employee.py
 Author:  Luke Xiong
 Date: 7/8/2020
 
@@ -13,20 +13,20 @@ from class_definitions.employee import Employee
 from _datetime import datetime
 
 
-class Salaried_Employee(Employee):
-    def __init__(self, lname, fname, address, phone_number, start_date, salary): #default values
+class Hourly_Employee(Employee):
+    def __init__(self, lname, fname, address, phone_number, start_date, hourly): #default values
         super().__init__(lname, fname, address, phone_number) #call bass constructor
         self._start_date = datetime.strptime(start_date, '%b %d %Y')
-        self._salary = salary
+        self._hourly = hourly
 
-    def give_raise(self, salary):
-        self._salary = salary
+    def give_raise(self, hourly):
+        self._hourly = hourly
 
     def display(self):
-        return self.last_name + ' '+ self.first_name + '\n' + self.address + '\n' + self.phone_number + '\n' + str(self._start_date) + '\n' + '$'+str(self._salary)
+        return self.last_name + ' '+ self.first_name + '\n' + self.address + '\n' + self.phone_number + '\n' + str(self._start_date) + '\n' + '$' + str(self._hourly)
 
     def str(self):
-        return self.last_name + ' '+ self.first_name + '\n' + self.address + '\n' + self.phone_number + '\n' + str(self._start_date) + '\n' + '$'+str(self._salary)
+        return self.last_name + ' '+ self.first_name + '\n' + self.address + '\n' + self.phone_number + '\n' + str(self._start_date) + '\n' + '$' + str(self._hourly)
 
     def __repr__(self):
         pass
@@ -35,11 +35,11 @@ if __name__=='__main__':
     #salaried_employee = $40k
     pass
 #Creates a SalariedEmployee object (select a meaningful name) with your name, start date today, starting salary $40,000.
-emp = Salaried_Employee('Ruiz', 'Matthew', '123 Main St, New York, NY', '555-111-3333', 'Jul 8 2020', 40000.00 ) #call the constructor
+emp = Hourly_Employee('Ruiz', 'Matthew', '123 Main St, New York, NY', '555-111-3333', 'Jul 8 2020', 10.00 ) #call the constructor
 #Displays the Employee object
 print(emp.display())
-#Changes salary to $45,000
-emp.give_raise(45000.00)
+#Changes hourly_pay to $12.00 an hour
+emp.give_raise(12.00)
 #Displays the Employee object
 print(emp.display())
 #Performs garbage
