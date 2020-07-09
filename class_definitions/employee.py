841732ca07f8893e6bbc54156f518fe3168557bc
lxiong515 <58718122+lxiong515@users.noreply.github.com>
2020-07-08 22:20:09 -0500

    updated employee.py to use with homework

--- class_definitions/employee.py
+++ class_definitions/employee.py
@@ -19,9 +19,11 @@ This program is
 
 class Employee:
 
-    def __init__(self, lname, fname):
+    def __init__(self, lname, fname, address, phone_number):
         self.last_name = lname
         self.first_name = fname
+        self.address = address
+        self.phone_number = phone_number
 
     def set_last_name(self, lname):
         self.last_name = lname
@@ -29,15 +31,21 @@ class Employee:
     def set_first_name(self, fname):
         self.first_name = fname
 
+    def set_address(self, address):
+        self.address = address
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
     def display(self):
-        return str(self.last_name) + ", " + str(self.first_name)
+        return str(self.last_name) + ", " + str(self.first_name) + '\n' + str(self.address) + '\n' + str(self.phone_number)
 
     def __str__(self):
-        return 'Person with last name ' + str(self.last_name) + ', first name ' + str(self.first_name)
+        return 'Person with last name ' + str(self.last_name) + ', first name ' + str(self.first_name) + '\n' + str(self.address) + '\n' + str(self.phone_number)
 #driver
-emp = Employee('Ruiz', 'Matthew') #call the constructor
-print(emp.display())
-emp.set_first_name('Matt')
-print(emp.display()) #display returns a str
-del emp #clean up
+#emp = Employee('Ruiz', 'Matthew', '123 Main St', '555-111-3333' ) #call the constructor
+#print(emp.display())
+#emp.set_first_name('Matt')
+#print(emp.display()) #display returns a str
+#del emp #clean up
 
